fd785c13843c442e3265bd39b051e532dc29eb1a
loneWolf <chotansharma@yahoo.com>
2022-01-02 01:31:21 -0500

    setting user repos

---
+++ assets/js/homepage.js
@@ -0,0 +1,9 @@
+var getUserRepos = function(user) {
+    fetch("https://api.github.com/users/" + user + "/repos").then(function(response) { 
+        response.json().then(function(data) {
+            console.log(data);
+    });
+});
+};
+
+getUserRepos("chotansharma");
